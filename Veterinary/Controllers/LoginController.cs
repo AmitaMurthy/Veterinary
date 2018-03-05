6e8ea3d63db95d4260ba8759fffb43730c4a3da2
AmitaMurthy <34549270+AmitaMurthy@users.noreply.github.com>
2018-03-05 13:15:49 -0600

    The project was uploaded after completion

---
+++ Veterinary/Controllers/LoginController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Veterinary.Models;
+
+namespace Veterinary.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(MyRegister register)
+        {
+            return View("ThankYouForRegistering", register);
+        }
+    }
+}
---
+++ Veterinary/dbo.Table.sql
@@ -0,0 +1,7 @@
+﻿CREATE TABLE [dbo].[Table]
+(
+	[Id] INT NOT NULL PRIMARY KEY, 
+    [Name] NCHAR(50) NOT NULL, 
+    [Email] NCHAR(50) NOT NULL, 
+    [Password] NCHAR(20) NOT NULL
+)
